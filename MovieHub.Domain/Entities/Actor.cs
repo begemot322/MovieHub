78a6effb077283dbdf777237560e87cd4e5e62b2
Николай <kolya.zaitsev6163@gmail.com>
2025-04-10 21:32:27 +0400

    Добавил сущность Актер

---
+++ MovieHub.Domain/Entities/Actor.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieHub.Domain.Entities;
+
+public class Actor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Bio { get; set; }
+    
+    public ICollection<ActorMovie> ActorMovies { get; set; } = new List<ActorMovie>();
+    
+    public ICollection<ActorLike> Likes { get; set; } = new List<ActorLike>();
+
+}
