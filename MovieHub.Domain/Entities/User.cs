253f098619cd20eab979540b2d767e71b1afe4da
Николай <kolya.zaitsev6163@gmail.com>
2025-04-10 21:34:51 +0400

    Добавил сущность пользователь

---
+++ MovieHub.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieHub.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    
+    public ICollection<ActorLike> ActorLikes { get; set; } = new List<ActorLike>();
+
+    public ICollection<MovieLike> MovieLikes { get; set; } = new List<MovieLike>();
+}
