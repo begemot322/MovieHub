43b8e281f3686cd79a9daecc2ad5d1c2e48b9265
Николай <kolya.zaitsev6163@gmail.com>
2025-04-10 21:45:29 +0400

    Добавил промежуточную таблицу для связи User и Movie.

---
+++ MovieHub.Domain/Entities/MovieLike.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieHub.Domain.Entities;
+
+public class MovieLike
+{
+    public int MovieId { get; set; }
+    public Movie Movie { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public DateTime LikedAt { get; set; } = DateTime.UtcNow;
+}
