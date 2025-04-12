118aee235ab0ee2bd5bef8f5e83ffda8d4f1ab9d
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:27:10 +0400

    использую паттерн unitofwork

---
+++ MovieHub.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using MovieHub.Application.Common.Interfaces.Repositories;
+
+namespace MovieHub.Application.Common.Interfaces;
+
+public interface IUnitOfWork
+{
+    IMovieRepository Movies { get; }
+    IActorRepository Actors { get; }
+    
+    Task<int> SaveChangesAsync();
+}
---
+++ MovieHub.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using MovieHub.Application.Common.Interfaces;
+using MovieHub.Application.Common.Interfaces.Repositories;
+using MovieHub.Infrastructure.Data.Repositories;
+
+namespace MovieHub.Infrastructure.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _db;
+    
+    private IMovieRepository _movieRepository;
+    private IActorRepository _actorRepository;
+
+
+    public UnitOfWork(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public IMovieRepository Movies => 
+        _movieRepository ??= new MovieRepository(_db);
+
+    public IActorRepository Actors =>
+        _actorRepository ??= new ActorRepository(_db);
+    
+    public void Dispose()
+    {
+        _db.Dispose();
+    }
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _db.SaveChangesAsync();
+    }
+
+}
