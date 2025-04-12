0ef8b0a2685a8d995469e046868a10b085a74bfd
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 14:18:22 +0400

    Создал контекст для базы данных

---
+++ MovieHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options)
+    {
+    }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<User> Users { get; set; }
+    
+    // Таблицы связи many-to-many
+    public DbSet<ActorMovie> ActorMovies { get; set; }
+    public DbSet<MovieLike> MovieLikes { get; set; }
+    public DbSet<ActorLike> ActorLikes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+    
+}
