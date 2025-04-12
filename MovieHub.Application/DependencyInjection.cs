9a7658b32e2948da06a3e6cac499cf96ea0d2e12
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 01:18:34 +0400

    Внедрил зависимость для сервисов в слое application

---
+++ MovieHub.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieHub.Application.Services.Implementation;
+using MovieHub.Application.Services.Interfaces;
+
+namespace MovieHub.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Сервисы
+        services.AddScoped<IMovieService, MovieService>();
+        services.AddScoped<IActorService, ActorService>();
+
+        return services;
+    }
+}
