466da240614a632ca25f4aefbaf95c91df4d4556
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 01:18:18 +0400

    Реализация контроллеров

---
+++ MovieHub.WebApi/Controllers/ActorController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieHub.Application.Dtos;
+using MovieHub.Application.Services.Interfaces;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ActorController : ControllerBase
+{
+    private readonly IActorService _actorService;
+
+    public ActorController(IActorService actorService)
+    {
+        _actorService = actorService;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Actor>>> GetAll()
+    {
+        var actors = await _actorService.GetAllAsync();
+        return Ok(actors);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Actor>> GetById(int id)
+    {
+        var actor = await _actorService.GetByIdAsync(id);
+        return Ok(actor);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> AddActor([FromBody] ActorDto actorDto)
+    {
+        var createdActor =  await _actorService.AddActorAsync(actorDto);
+        return CreatedAtAction(nameof(GetById), new { id = createdActor.Id }, createdActor);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _actorService.DeleteActorAsync(id);
+        return NoContent();
+    }
+    
+    [HttpGet("{actorId}/movies")]
+    public async Task<ActionResult<IEnumerable<Movie>>> GetMoviesByActorId(int actorId)
+    {
+        var movies = await _actorService.GetMoviesByActorIdAsync(actorId);
+        
+        return Ok(movies);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateMovie(int id, [FromBody] ActorDto actorDto)
+    {
+        await _actorService.UpdateActorAsync(id, actorDto);
+        return NoContent();
+    }
+}
---
+++ MovieHub.WebApi/Controllers/MovieController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieHub.Application.Common;
+using MovieHub.Application.Dtos;
+using MovieHub.Application.Filters;
+using MovieHub.Application.Services.Interfaces;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MovieController : ControllerBase
+{
+    private readonly IMovieService _movieService;
+
+    public MovieController(IMovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Movie>>> GetAll([FromQuery] MovieFilter? filter, [FromQuery] SortParams? sortParams)
+    {
+        var movies = await _movieService.GetAllAsync(filter, sortParams);
+        return Ok(movies);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Movie>> GetById(int id)
+    {
+        var movie = await _movieService.GetByIdAsync(id);
+        return Ok(movie);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddMovie([FromBody] MovieDto movieDto)
+    {
+        var createdMovie = await _movieService.AddMovieAsync(movieDto);
+
+        return CreatedAtAction(nameof(GetById), new { id = createdMovie.Id }, createdMovie);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _movieService.DeleteMovieAsync(id);
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateMovie(int id, [FromBody] MovieDto movieDto)
+    {
+        await _movieService.UpdateMovieAsync(id, movieDto);
+        return NoContent();
+    }
+}
