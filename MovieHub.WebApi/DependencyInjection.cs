3223eeefe24485257c7b339e5d28e3d421e2ae63
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:36:56 +0400

    Добавил servic extension для слоя web.

---
+++ MovieHub.WebApi/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using MovieHub.WebApi.Filters;
+
+namespace MovieHub.WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddControllers(options =>
+            {
+                options.Filters.Add(new ApiExceptionFilter());
+            })
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();   
+
+        return services;
+    }
+}
