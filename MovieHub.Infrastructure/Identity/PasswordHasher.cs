1a58cb18361fd4e26b5c1b876dd4e46847e958e2
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 03:27:59 +0400

    Реализация интерфейса. один генерирует пароль другой проверяет

---
+++ MovieHub.Infrastructure/Identity/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using MovieHub.Application.Common.Interfaces.Identity;
+
+namespace MovieHub.Infrastructure.Identity;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Generate(string password)
+    {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    }
+
+    public bool Verify(string password, string HashPassword)
+    {
+        return BCrypt.Net.BCrypt.EnhancedVerify(password, HashPassword);
+    }
+}
