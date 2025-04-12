489e63f1d64c2edc72d964a765151a175bc08c6f
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:28:49 +0400

    Класс для фильтрации фильмов

---
+++ MovieHub.Application/Filters/MovieFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieHub.Application.Filters;
+
+public class MovieFilter
+{
+    public string? Title { get; set; }
+    public int? MinLikesCount { get; set; }
+    public int? MaxLikesCount { get; set; }
+}
