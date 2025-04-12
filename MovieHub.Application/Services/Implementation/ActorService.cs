7af2a78d20e1e4918817e707b78fa4e3b60c24b1
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 01:17:01 +0400

    Реализация сервисов для основных сущностей

---
+++ MovieHub.Application/Services/Implementation/ActorService.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MovieHub.Application.Common.Exceptions;
+using MovieHub.Application.Common.Interfaces;
+using MovieHub.Application.Dtos;
+using MovieHub.Application.Services.Interfaces;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Application.Services.Implementation;
+
+public class ActorService : IActorService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ActorService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<IEnumerable<Actor>> GetAllAsync()
+    {
+        var actors = await _unitOfWork.Actors.GetAllAsync();
+        
+        if (!actors.Any())
+            throw new NotFoundException("Актёры не найдены");
+
+        return actors;
+    }
+    
+    public async Task<Actor> GetByIdAsync(int id)
+    {
+        var actor = await _unitOfWork.Actors.GetByIdAsync(id);
+
+        if (actor == null)
+            throw new NotFoundException($"Актёр с Id {id} не найден");
+
+        return actor;
+    }
+    
+    public async Task<Actor> AddActorAsync(ActorDto actorDto)
+    {
+        if (string.IsNullOrWhiteSpace(actorDto.Name) || string.IsNullOrWhiteSpace(actorDto.Surname))
+            throw new ValidationException("Имя и фамилия актёра не могут быть пустыми");
+
+        var exists = await _unitOfWork.Actors.ExistsAsync(a =>
+            a.Name == actorDto.Name && a.Surname== actorDto.Surname);
+        if (exists)
+            throw new DuplicateException($"Актёр с именем {actorDto.Name} и фамилией {actorDto.Surname} уже существует");
+
+        var actor = new Actor()
+        {
+            Name = actorDto.Name,
+            Surname = actorDto.Surname,
+            Bio = actorDto.Bio
+        };
+
+        await _unitOfWork.Actors.AddAsync(actor);
+        await _unitOfWork.SaveChangesAsync();
+
+        return actor;
+    }
+    
+    public async Task DeleteActorAsync(int id)
+    {
+        var actor = await _unitOfWork.Actors.GetByIdAsync(id);
+
+        if (actor == null)
+            throw new NotFoundException($"Актёр с Id {id} не найден");
+
+        _unitOfWork.Actors.Remove(actor);
+        await _unitOfWork.SaveChangesAsync();
+    }
+    public async Task<IEnumerable<Movie>> GetMoviesByActorIdAsync(int actorId)
+    {
+        var actor = await _unitOfWork.Actors.GetByIdAsync(actorId);
+
+        if (actor == null)
+            throw new NotFoundException($"Актёр с Id {actorId} не найден");
+        
+        var actorMovies = await _unitOfWork.Actors.GetMoviesByActorIdAsync(actorId); 
+    
+        if (!actorMovies.Any())
+            throw new NotFoundException($"Актер с Id {actorId} не найден или не участвует в фильмах");
+
+        return actorMovies;
+    }
+
+    public async Task UpdateActorAsync(int id, ActorDto actorDto)
+    {
+        if (string.IsNullOrWhiteSpace(actorDto.Name) || string.IsNullOrWhiteSpace(actorDto.Surname))
+            throw new ValidationException("Имя и фамилия актёра не могут быть пустыми");
+        
+        var actor = await _unitOfWork.Actors.GetByIdAsync(id);
+
+        if (actor == null)
+            throw new NotFoundException($"Актёр с Id {id} не найден");
+        
+        var exists = await _unitOfWork.Actors.ExistsAsync(a =>
+            a.Name == actorDto.Name && a.Surname == actorDto.Surname && a.Id != id);
+        if (exists)
+            throw new DuplicateException($"Актёр с именем {actorDto.Name} и фамилией {actorDto.Surname} уже существует");
+
+        actor.Name = actorDto.Name;
+        actor.Surname = actorDto.Surname;
+        actor.Bio = actorDto.Bio;
+        
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
---
+++ MovieHub.Application/Services/Implementation/MovieService.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MovieHub.Application.Common;
+using MovieHub.Application.Common.Exceptions;
+using MovieHub.Application.Common.Interfaces;
+using MovieHub.Application.Common.Interfaces.Repositories;
+using MovieHub.Application.Dtos;
+using MovieHub.Application.Filters;
+using MovieHub.Application.Services.Interfaces;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Application.Services.Implementation;
+
+public class MovieService : IMovieService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public MovieService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IEnumerable<Movie>> GetAllAsync(MovieFilter? filter = null, SortParams? sortParams = null)
+    {
+        var movies = await _unitOfWork.Movies.GetAllAsync(filter, sortParams);
+
+        if (!movies.Any())
+            throw new NotFoundException("Фильмы не найдены");
+
+        return movies;
+    }
+
+    public async Task<Movie> GetByIdAsync(int id)
+    {
+        var movie = await _unitOfWork.Movies.GetByIdAsync(id);
+
+        if (movie == null)
+            throw new NotFoundException($"Фильм с Id {id} не найден");
+
+        return movie;
+    }
+
+    public async Task<Movie> AddMovieAsync(MovieDto movieDto)
+    {
+        if (string.IsNullOrWhiteSpace(movieDto.Title))
+            throw new ValidationException("Название фильма не может быть пустым");
+        
+        var exists = await _unitOfWork.Movies.ExistsAsync(m => m.Title == movieDto.Title);
+        if (exists)
+            throw new DuplicateException($"Фильм с названием {movieDto.Title} уже существует");
+        
+        var movie = new Movie
+        {
+            Title = movieDto.Title,
+            ReleaseDate = movieDto.ReleaseDate,
+            Description = movieDto.Description,
+        };
+        
+        var actors = await _unitOfWork.Actors.GetByIdsAsync(movieDto.ActorIds);
+        
+        if (actors.Count != movieDto.ActorIds.Count)
+            throw new NotFoundException("Один или несколько актеров не найдены");
+        
+        foreach (var actor in actors)
+        {
+            movie.ActorMovies.Add(new ActorMovie
+            {
+                Actor = actor,
+                Movie = movie
+            });
+        }
+        await _unitOfWork.Movies.AddAsync(movie);
+        await _unitOfWork.SaveChangesAsync(); 
+        
+        return movie;
+    }
+
+    public async Task DeleteMovieAsync(int id)
+    {
+        var movie = await _unitOfWork.Movies.GetByIdAsync(id);
+
+        if (movie == null)
+            throw new NotFoundException($"Фильм с Id {id} не найден");
+
+        _unitOfWork.Movies.Remove(movie);
+        await _unitOfWork.SaveChangesAsync(); 
+    }
+
+    public async Task UpdateMovieAsync(int id, MovieDto movieDto)
+    {
+        if (string.IsNullOrWhiteSpace(movieDto.Title))
+            throw new ValidationException("Название фильма не может быть пустым");
+        
+        var movie = await _unitOfWork.Movies.GetByIdAsync(id);
+        if (movie == null)
+            throw new NotFoundException($"Фильм с Id {id} не найден");
+        
+        var exists = await _unitOfWork.Movies.ExistsAsync(m => m.Title == movieDto.Title && m.Id != id);
+        if (exists)
+            throw new DuplicateException($"Фильм с названием {movieDto.Title} уже существует");
+        
+        movie.Title = movieDto.Title;
+        movie.ReleaseDate = movieDto.ReleaseDate;
+        movie.Description = movieDto.Description;
+        
+        var actors = await _unitOfWork.Actors.GetByIdsAsync(movieDto.ActorIds);
+        
+        if (actors.Count != movieDto.ActorIds.Count)
+            throw new NotFoundException("Один или несколько актеров не найдены");
+        
+        movie.ActorMovies.Clear();
+        
+        foreach (var actor in actors)
+        {
+            movie.ActorMovies.Add(new ActorMovie
+            {
+                ActorId = actor.Id,
+                MovieId = movie.Id
+            });
+        }
+        
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
