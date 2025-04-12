8186df48ccb38635be8c18a300d5084203805d2f
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 01:16:25 +0400

    Добавил дтошки для получения данных с сервера

---
+++ MovieHub.Application/Dtos/ActorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieHub.Application.Dtos;
+
+public class ActorDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Bio { get; set; }
+}
---
+++ MovieHub.Application/Dtos/MovieDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieHub.Application.Dtos;
+
+public class MovieDto
+{
+    public string Title { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string Description { get; set; }
+    public List<int> ActorIds { get; set; } = new();
+}
