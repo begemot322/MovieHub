4a25120fa87dd7dfa7bda12886de7c8b710d352b
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 14:22:48 +0400

    Добавил подключение для базы данных

---
+++ MovieHub.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieHub.Infrastructure.Data;
+using MovieHub.Infrastructure.Data.Repositories;
+
+namespace MovieHub.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabase(configuration);
+
+        return services;
+    }
+    
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connString));
+        
+        services.AddScoped<DbInitializer>();
+        
+        return services;
+    }
+}
