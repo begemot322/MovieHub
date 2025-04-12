3bf839137494081d02a754ab84cef4401fe24c91
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:05:59 +0400

    добавил поля фамилие

--- MovieHub.Domain/Entities/Actor.cs
+++ MovieHub.Domain/Entities/Actor.cs
@@ -4,6 +4,7 @@ public class Actor
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Surname { get; set; }
     public string Bio { get; set; }
     
     public ICollection<ActorMovie> ActorMovies { get; set; } = new List<ActorMovie>();
