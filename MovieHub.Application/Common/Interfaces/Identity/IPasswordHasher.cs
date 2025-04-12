2bf5b3d474df0443027b2d5623fa07714577a48b
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 03:27:36 +0400

    Интерфейс для хеширования пароля

---
+++ MovieHub.Application/Common/Interfaces/Identity/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieHub.Application.Common.Interfaces.Identity;
+
+public interface IPasswordHasher
+{
+    string Generate(string password);
+    bool Verify(string password, string HashPassword);
+}
