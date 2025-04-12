dd2e70bad6bbc22b3c899c9636e17f825c2b2dd0
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:42:24 +0400

    Добавил параметры сортировки

---
+++ MovieHub.Application/Common/SortParams.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieHub.Application.Common;
+
+public class SortParams
+{
+    public string? OrderBy { get; set; }
+    public SortDirection? SortDirection { get; set; }
+}
+
+public enum SortDirection
+{
+    Ascending, 
+    Descending
+}
+
