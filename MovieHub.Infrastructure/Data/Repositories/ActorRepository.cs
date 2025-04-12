722ac380ef92267938bfb00dfc0225a92845d847
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:26:37 +0400

    Реализация репозиториев

---
+++ MovieHub.Infrastructure/Data/Repositories/ActorRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MovieHub.Application.Common.Interfaces.Repositories;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Infrastructure.Data.Repositories;
+
+public class ActorRepository : IActorRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public ActorRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public async Task<IEnumerable<Actor>> GetAllAsync()
+    {
+        return await _db.Actors
+            .Include(a => a.ActorMovies)
+                .ThenInclude(am => am.Movie)
+            .Include(a => a.ActorLikes)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<Actor?> GetByIdAsync(int id)
+    {
+        return await _db.Actors
+            .Include(a => a.ActorMovies)
+                .ThenInclude(am => am.Movie)
+            .Include(a => a.ActorLikes)
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task AddAsync(Actor actor)
+    {
+        await _db.Actors.AddAsync(actor);
+    }
+
+    public void Remove(Actor actor)
+    {
+        _db.Actors.Remove(actor);
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<Actor, bool>> predicate)
+    {
+        return await _db.Actors.AnyAsync(predicate);
+    }
+
+    public async Task<List<Actor>> GetByIdsAsync(List<int> ids)
+    {
+        return await _db.Actors
+            .Where(a => ids.Contains(a.Id))
+            .ToListAsync();
+    }
+}
---
+++ MovieHub.Infrastructure/Data/Repositories/MovieRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MovieHub.Application.Common;
+using MovieHub.Application.Common.Interfaces.Repositories;
+using MovieHub.Application.Filters;
+using MovieHub.Domain.Entities;
+using MovieHub.Infrastructure.Extensions;
+
+namespace MovieHub.Infrastructure.Data.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public MovieRepository(ApplicationDbContext db)
+    {
+        _db = db;   
+    }
+
+    public async Task<IEnumerable<Movie>> GetAllAsync(MovieFilter? movieFilter = null,
+        SortParams? sortParams = null)
+    {
+        return await _db.Movies
+            .Sort(sortParams)
+            .Filter(movieFilter)
+            .Include(m => m.MovieLikes)
+            .Include(m => m.ActorMovies)
+                .ThenInclude(am => am.Actor)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+    
+    public async Task<Movie?> GetByIdAsync(int id)
+    {
+        return await _db.Movies
+            .Include(m => m.MovieLikes)
+            .Include(m => m.ActorMovies)
+            .ThenInclude(am => am.Actor)
+            .FirstOrDefaultAsync(m => m.Id == id);
+    }
+
+    public async Task AddAsync(Movie movie)
+    {
+        await _db.Movies.AddAsync(movie);
+    }
+
+    public void Remove(Movie movie)
+    {
+        _db.Movies.Remove(movie);
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<Movie, bool>> predicate)
+    {
+        return await _db.Movies.AnyAsync(predicate);
+    }
+}
