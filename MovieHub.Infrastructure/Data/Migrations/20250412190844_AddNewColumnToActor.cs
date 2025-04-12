0e0f2ce4c9d7acfc70c5d3ff2d43842029849d3e
Николай <kolya.zaitsev6163@gmail.com>
2025-04-12 23:17:02 +0400

    Миграция для нового столбца

---
+++ MovieHub.Infrastructure/Data/Migrations/20250412190844_AddNewColumnToActor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieHub.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumnToActor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Actors",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Actors");
+        }
+    }
+}
