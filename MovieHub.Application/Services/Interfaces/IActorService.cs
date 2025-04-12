d83edf65528eec74358ed3c0770ca4454475a7ee
Николай <kolya.zaitsev6163@gmail.com>
2025-04-13 01:16:46 +0400

    добавил интерфейсы для сервисов

---
+++ MovieHub.Application/Services/Interfaces/IActorService.cs
@@ -0,0 +1,14 @@
+﻿using MovieHub.Application.Dtos;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Application.Services.Interfaces;
+
+public interface IActorService
+{
+    Task<IEnumerable<Actor>> GetAllAsync();
+    Task<Actor> GetByIdAsync(int id);
+    Task<Actor> AddActorAsync(ActorDto actorDto);
+    Task DeleteActorAsync(int id);
+    Task<IEnumerable<Movie>> GetMoviesByActorIdAsync(int actorId);
+    Task UpdateActorAsync(int id, ActorDto actorDto);
+}
---
+++ MovieHub.Application/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MovieHub.Application.Common;
+using MovieHub.Application.Common.Exceptions;
+using MovieHub.Application.Dtos;
+using MovieHub.Application.Filters;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Application.Services.Interfaces;
+
+public interface IMovieService
+{
+    Task<IEnumerable<Movie>> GetAllAsync(MovieFilter? filter = null, SortParams? sortParams = null);
+
+    Task<Movie> GetByIdAsync(int id);
+
+    Task<Movie> AddMovieAsync(MovieDto movieDto);
+
+    Task DeleteMovieAsync(int id);
+
+    Task UpdateMovieAsync(int id, MovieDto movieDto);
+
+}
