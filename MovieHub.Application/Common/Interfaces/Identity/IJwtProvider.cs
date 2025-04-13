aeab7d9ea26d93ac22f3210d9e8128dfc5d17606
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 00:20:17 +0400

    Генерация токена jwt

---
+++ MovieHub.Application/Common/Interfaces/Identity/IJwtProvider.cs
@@ -0,0 +1,8 @@
+﻿using MovieHub.Domain.Entities;
+
+namespace MovieHub.Application.Common.Interfaces.Identity;
+
+public interface IJwtProvider
+{
+    string GenerateToken(User user);
+}
---
+++ MovieHub.Infrastructure/Identity/Auth/JwtProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieHub.Application.Common.Interfaces.Identity;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Infrastructure.Identity.Auth;
+
+public class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _options;
+    
+    public JwtProvider(IOptions<JwtOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        Claim[] claims =
+        {
+            new Claim("userId", user.Id.ToString()),
+        };
+
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+            SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            issuer: _options.Issuer,
+            audience: _options.Audience,
+            expires: DateTime.UtcNow.AddHours(_options.ExpiresHours),
+            signingCredentials: signingCredentials);
+
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return tokenValue;
+    }
+}
