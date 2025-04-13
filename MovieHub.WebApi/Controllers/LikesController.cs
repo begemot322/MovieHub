d23dbeab7f66d690689126ae79eb73dc86f18def
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 00:27:25 +0400

    Реализация контроллера для лайков

---
+++ MovieHub.WebApi/Controllers/LikesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieHub.Application.Services.Interfaces;
+
+namespace MovieHub.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class LikesController : ControllerBase
+{
+    private readonly ILikeService _likeService;
+    
+    public LikesController(ILikeService likeService)
+    {
+        _likeService = likeService;
+    }
+    
+    [HttpPost("movies/{movieId:int}")]
+    public async Task<IActionResult> LikeMovie(int movieId)
+    {
+        await _likeService.LikeMovieAsync(movieId);
+        return Ok();
+    }
+    
+    [HttpPost("actors/{actorId:int}")]
+    public async Task<IActionResult> LikeActor(int actorId)
+    {
+        await _likeService.LikeActorAsync(actorId);
+        return Ok();
+    }
+}
