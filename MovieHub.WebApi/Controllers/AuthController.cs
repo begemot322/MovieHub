308917abbbdcd1fc6f96ebe65cf009a765e668b1
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 00:26:21 +0400

    Реализация контроллера для регистрации и входа

---
+++ MovieHub.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieHub.Application.Dtos.Authentication;
+using MovieHub.Application.Services.Interfaces;
+
+namespace MovieHub.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _userService;
+    
+    public AuthController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
+    {
+        await _userService.RegisterUserAsync(request);
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginUserRequest request)
+    {
+        var token = await _userService.LoginAsync(request);
+        Response.Cookies.Append("SecurityCookies", token);
+        return Ok(token);
+    }
+        
+}
