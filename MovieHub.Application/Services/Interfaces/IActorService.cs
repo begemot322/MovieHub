34368591e61e47dafee8bff72094f6faf4ac8812
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 00:22:00 +0400

    Добавил параметры для сортировки

--- MovieHub.Application/Services/Interfaces/IActorService.cs
+++ MovieHub.Application/Services/Interfaces/IActorService.cs
@@ -1,11 +1,12 @@
-﻿using MovieHub.Application.Dtos;
+﻿using MovieHub.Application.Common;
+using MovieHub.Application.Dtos;
 using MovieHub.Domain.Entities;
 
 namespace MovieHub.Application.Services.Interfaces;
 
 public interface IActorService
 {
-    Task<IEnumerable<Actor>> GetAllAsync();
+    Task<IEnumerable<Actor>> GetAllAsync(SortParams? sortParams = null);
     Task<Actor> GetByIdAsync(int id);
     Task<Actor> AddActorAsync(ActorDto actorDto);
     Task DeleteActorAsync(int id);
