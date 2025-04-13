82c8d2181ba8487d36659d09c8c86dd4bb03c95b
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 02:30:25 +0400

    Добавил page result

---
+++ MovieHub.Application/Common/PagedResult.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieHub.Application.Common;
+
+public class PagedResult<T>
+{
+    public List<T> Items { get; }
+    public int TotalCount { get; }
+    public int Page { get; }      
+    public int PageSize { get; }   
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize); // Всего страниц
+
+    public PagedResult(List<T> items, int totalCount, int page, int pageSize)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        Page = page;
+        PageSize = pageSize;
+    }
+}
