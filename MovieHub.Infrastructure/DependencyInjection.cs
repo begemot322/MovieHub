7c18769a9f09ffb93f7b152cd066f2bcabcdb001
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 00:21:49 +0400

    Добавил Jwt аунтефикацию

--- MovieHub.Infrastructure/DependencyInjection.cs
+++ MovieHub.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using MovieHub.Application.Common.Interfaces;
-using MovieHub.Application.Common.Interfaces.Repositories;
+using MovieHub.Application.Common.Interfaces.Identity;
 using MovieHub.Infrastructure.Data;
-using MovieHub.Infrastructure.Data.Repositories;
+using MovieHub.Infrastructure.Identity.Auth;
+using MovieHub.Infrastructure.Identity;
 
 namespace MovieHub.Infrastructure;
 
@@ -13,6 +18,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDatabase(configuration);
+        services.AddAuth(configuration);
 
         return services;
     }
@@ -26,6 +32,48 @@ public static class DependencyInjection
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         
+        return services;
+    }
+
+    private static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+
+        var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtOptions.Audience,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions!.SecretKey)),
+                    RoleClaimType = ClaimTypes.Role,
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["SecurityCookies"];
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        
+        services.AddScoped<IJwtProvider, JwtProvider>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<IUserContext, UserContext>();
+
         return services;
     }
 }
