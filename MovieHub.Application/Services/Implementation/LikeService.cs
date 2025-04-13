e7e8ccce75d1a57fd227bcded0cbed9db8143656
Николай <kolya.zaitsev6163@gmail.com>
2025-04-14 00:27:01 +0400

    Добавил сервис для поставки лайка фильму или актеру

---
+++ MovieHub.Application/Services/Implementation/LikeService.cs
@@ -0,0 +1,61 @@
+﻿using MovieHub.Application.Common.Exceptions;
+using MovieHub.Application.Common.Interfaces;
+using MovieHub.Application.Services.Interfaces;
+using MovieHub.Domain.Entities;
+
+namespace MovieHub.Application.Services.Implementation;
+
+public class LikeService : ILikeService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserContext _userContext;
+    
+    public LikeService(IUnitOfWork unitOfWork, IUserContext userContext)
+    {
+        _unitOfWork = unitOfWork;
+        _userContext = userContext;
+    }
+
+    public async Task LikeMovieAsync(int movieId)
+    {
+        var user = await GetUserWithLikesAsync();
+    
+        if (user.MovieLikes.Any(ml => ml.MovieId == movieId))
+            throw new DuplicateException("Вы уже лайкнули этот фильм");
+
+        var movie = await _unitOfWork.Movies.GetByIdAsync(movieId);
+        if (movie == null)
+            throw new NotFoundException($"Фильм с Id {movieId} не найден");
+
+        user.MovieLikes.Add(new MovieLike { MovieId = movieId, UserId = user.Id, LikedAt = DateTime.UtcNow });
+        await _unitOfWork.SaveChangesAsync();
+    }
+
+    public async Task LikeActorAsync(int actorId)
+    {
+        var user = await GetUserWithLikesAsync();
+    
+        if (user.ActorLikes.Any(al => al.ActorId == actorId))
+            throw new DuplicateException("Вы уже лайкнули этого актера");
+
+        var actor = await _unitOfWork.Actors.GetByIdAsync(actorId);
+        if (actor == null)
+            throw new NotFoundException($"Актер с Id {actorId} не найден");
+
+        user.ActorLikes.Add(new ActorLike { ActorId = actorId, UserId = user.Id, LikedAt = DateTime.UtcNow });
+        await _unitOfWork.SaveChangesAsync();
+    }
+    
+    private async Task<User> GetUserWithLikesAsync()
+    {
+        var userId = _userContext.GetCurrentUserId();
+        if (userId == null)
+            throw new UnauthorizedAccessException("Пользователь не авторизован");
+
+        var user = await _unitOfWork.Users.GetUserWithLikesAsync(userId.Value);
+        if (user == null)
+            throw new NotFoundException($"Пользователь с Id {userId} не найден");
+
+        return user;
+    }
+}
---
+++ MovieHub.Application/Services/Interfaces/ILikeService.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieHub.Application.Services.Interfaces;
+
+public interface ILikeService
+{
+    Task LikeMovieAsync(int movieId);
+    Task LikeActorAsync(int actorId);
+}
